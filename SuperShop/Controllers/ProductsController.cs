ef5428df9c3418119aa78e3cb430a8d15d76df95
Gabriel <costa.gabriel.gabriel@gmail.com>
2022-07-25 09:20:36 +0100

    Add and Edit images, images on API Products, User on API Products

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using SuperShop.Data;
 using SuperShop.Data.Entities;
 using SuperShop.Helpers;
+using SuperShop.Models;
 
 namespace SuperShop.Controllers
 {
@@ -60,10 +62,36 @@ namespace SuperShop.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Price,ImageUrl,LastPurchase,LastSale,IsAvailabe,Stock")] Product product)
+        public async Task<IActionResult> Create(ProductViewModel model)
         {
             if (ModelState.IsValid)
             {
+
+                var path = string.Empty;
+
+                if(model.ImageFile != null && model.ImageFile.Length > 0)
+                {
+                    var guid = Guid.NewGuid().ToString();
+                    var file = $"{guid}.jpg";
+
+                    path = Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        "wwwroot\\images\\products",
+                        file
+                        );
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await model.ImageFile.CopyToAsync(stream);
+                    }
+
+                    path = $"~/images/products/{file}";
+
+
+                }
+
+                var product = this.ToProduct(model, path);
+
                 // TODO: MODIFICAR PARA O USER QUE ESTIVER LOGADO
                 product.User = await _userHelper.GetUserByEmailAsync("gabriel@gmail.com");
 
@@ -71,7 +99,22 @@ namespace SuperShop.Controllers
 
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(model);
+        }
+
+        private Product ToProduct(ProductViewModel model, string path)
+        {
+            return new Product { 
+                Id = model.Id,
+                ImageUrl = path,
+                IsAvailabe = model.IsAvailabe,
+                LastPurchase = model.LastPurchase,
+                LastSale = model.LastSale,
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock,
+                User = model.User
+            };
         }
 
         // GET: Products/Edit/5
@@ -88,7 +131,25 @@ namespace SuperShop.Controllers
             {
                 return NotFound();
             }
-            return View(product);
+
+            var model = this.ToProductViewModel(product);
+
+
+            return View(model);
+        }
+
+        private ProductViewModel ToProductViewModel(Product product)
+        {
+            return new ProductViewModel {
+                Id = product.Id,
+                IsAvailabe = product.IsAvailabe,
+                LastPurchase = product.LastPurchase,
+                LastSale = product.LastSale,
+                ImageUrl = product.ImageUrl,
+                Name = product.Name,
+                Stock = product.Stock,
+                User = product.User
+            };
         }
 
         // POST: Products/Edit/5
@@ -96,17 +157,37 @@ namespace SuperShop.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,ImageUrl,LastPurchase,LastSale,IsAvailabe,Stock")] Product product)
+        public async Task<IActionResult> Edit(ProductViewModel model)
         {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
 
             if (ModelState.IsValid)
             {
                 try
                 {
+
+                    var path = model.ImageUrl;
+
+                    if(model.ImageFile != null && model.ImageFile.Length > 0)
+                    {
+                        var guid = Guid.NewGuid().ToString();
+                        var file = $"{guid}.jpg";
+
+                        path = Path.Combine(
+                            Directory.GetCurrentDirectory(),
+                            "wwwroot\\images\\products",
+                            file
+                            );
+
+                        using(var stream = new FileStream(path, FileMode.Create))
+                        {
+                            await model.ImageFile.CopyToAsync(stream);
+                        }
+
+                        path = $"~/images/products/{file}";
+                    }
+
+                    var product = this.ToProduct(model, path);
+
                     // TODO: MODIFICAR PARA O USER QUE ESTIVER LOGADO
                     product.User = await _userHelper.GetUserByEmailAsync("gabriel@gmail.com");
                     await _productRepository.UpdateAsync(product);
@@ -114,7 +195,7 @@ namespace SuperShop.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!await _productRepository.ExistAsync(product.Id))
+                    if (!await _productRepository.ExistAsync(model.Id))
                     {
                         return NotFound();
                     }
@@ -125,7 +206,7 @@ namespace SuperShop.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
+            return View(model);
         }
 
         // GET: Products/Delete/5
--- SuperShop/Data/ProductRepository.cs
+++ SuperShop/Data/ProductRepository.cs
@@ -1,12 +1,22 @@
-﻿using SuperShop.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+using System.Linq;
 
 namespace SuperShop.Data
 {
     public class ProductRepository : GenericRepository<Product>, IProductsRepository
     {
+        public DataContext _context { get; set; }
+
         public ProductRepository(DataContext context) : base(context)
         {
-
+            _context = context;
         }
+
+        public IQueryable GetAllWithUsers()
+        {
+            return _context.Products.Include(p => p.User);
+        }
+        
     }
 }
