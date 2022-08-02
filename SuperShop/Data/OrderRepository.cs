8974f39123d77d27d3fb53205e0b3fae3318a375
Gabriel <costa.gabriel.gabriel@gmail.com>
2022-08-02 10:32:27 +0100

    Decrease, Increase, delete items

--- SuperShop/Data/OrderRepository.cs
+++ SuperShop/Data/OrderRepository.cs
@@ -118,5 +118,18 @@ namespace SuperShop.Data
                 await _context.SaveChangesAsync();
             }
         }
+
+        public async Task DeleteDetailTempAsync(int id)
+        {
+            var orderDetailTemp = await _context.OrderDetailsTemp.FindAsync(id);
+
+            if(orderDetailTemp == null)
+            {
+                return;
+            }
+
+            _context.OrderDetailsTemp.Remove(orderDetailTemp);
+            await _context.SaveChangesAsync();
+        }
     }
 }
